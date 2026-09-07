642edc665d116c2ba60c5f5e0cd5c765780eca03
ilya petrichshev <ilya.petrichshev.dev@gmail.com>
2026-09-07 21:14:32 +0500

    responsive design added

--- js/main.js
+++ js/main.js
@@ -4,3 +4,13 @@ const sloganText = document.querySelector(".slogan__text");
 slogan.addEventListener("mouseenter", () => {
     sloganText.classList.add("is-visible");
 });
+
+const cards = document.querySelectorAll(".card");
+
+if (window.matchMedia("(hover: none) and (pointer: coarse)").matches) {
+  cards.forEach((card) => {
+    card.addEventListener("click", () => {
+      card.classList.add("active");
+    });
+  });
+}
